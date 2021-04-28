2483d56145b0382b86f6fef941c37222fe547869
AntoniCabral <cabral.antoni@hotmail.fr>
2021-04-28 19:03:57 +0200

    ajout du background

--- main.py
+++ main.py
@@ -1,3 +1,27 @@
 import pygame
 pygame.init()
 
+# Fenetre PyGame
+
+pygame.display.set_caption("L'arbre")
+screen = pygame.display.set_mode((1080, 680))
+
+# init
+background = pygame.image.load("assets/bg.jpg")
+
+
+# Game Loop
+
+running = True
+
+
+while running:
+    #applique l'arrière plan
+    screen.blit(background, (0, -250))
+
+    #Update ecran
+    pygame.display.flip()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            pygame.quit()
