d3ddf6cdb514902af4813a97d2b79a3b9c49f181
AntoniCabral <cabral.antoni@hotmail.fr>
2021-04-28 20:32:08 +0200

    creation joueur

--- main.py
+++ main.py
@@ -1,25 +1,50 @@
 import pygame
+
 pygame.init()
+
+class Game :
+
+    def __init__(self):
+        self.player = Player()
+
+
+class Player(pygame.sprite.Sprite):
+
+    def __init__(self):
+        super().__init__()
+        self.health = 100
+        self.maxHealth = 100
+        self.attack = 10
+        self.velocity = 5
+        self.image = pygame.image.load("assets/player.png")
+        self.rect = self.image.get_rect()
+        self.rect.x = 450
+        self.rect.y = 450
+
 
 # Fenetre PyGame
 
 pygame.display.set_caption("L'arbre")
 screen = pygame.display.set_mode((1080, 680))
 
-# init
+# importe et charge arriere plan
 background = pygame.image.load("assets/bg.jpg")
+# charge Game
+game = Game()
 
 
 # Game Loop
 
 running = True
 
-
 while running:
-    #applique l'arrière plan
+    # draw l'arrière plan
     screen.blit(background, (0, -250))
+    # draw joueur
+    screen.blit(game.player.image, game.player.rect)
 
-    #Update ecran
+    # Update ecran
+
     pygame.display.flip()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
